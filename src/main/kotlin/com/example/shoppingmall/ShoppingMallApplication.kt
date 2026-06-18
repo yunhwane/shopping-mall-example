d0d5cd4a602beb9cfdbda92ce1c525d8f552c011
jeonyunhwan <wjsdbsghks96@dgu.ac.kr>
2026-06-18 15:21:19 +0900

    initial project

---
+++ src/main/kotlin/com/example/shoppingmall/ShoppingMallApplication.kt
@@ -0,0 +1,11 @@
+package com.example.shoppingmall
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ShoppingMallApplication
+
+fun main(args: Array<String>) {
+    runApplication<ShoppingMallApplication>(*args)
+}
